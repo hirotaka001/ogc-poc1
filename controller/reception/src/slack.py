19d1fefb96f08cd82446ee11e80c86b964a86aa6
urabe <urabe0225@gmail.com>
2018-10-18 18:11:59 +0900

    modified slack.py and test_slack.py

--- controller/reception/src/slack.py
+++ controller/reception/src/slack.py
@@ -5,6 +5,7 @@ import json
 
 logger = getLogger(__name__)
 
+
 def send_message_to_slack(webhook, room):
     message = f'来客がいらっしゃいました。{room}までご案内いたしております'
     logger.info(message)
@@ -12,7 +13,12 @@ def send_message_to_slack(webhook, room):
         "text":    message
     }
     r = requests.post(webhook, data=json.dumps(payload_dic))
-    logger.info(r)
-    if r.status_code != 200:
+    try:
+        r.raise_for_status()
+        return r
+    except requests.exceptions.HTTPError as e:
         logger.error(f'Slack API return Error : {str(r.status_code)}')
-    return r
+        logger.error(type(e))
+        raise e
+    else:
+        logger.info(r)
--- controller/reception/tests/test_slack.py
+++ controller/reception/tests/test_slack.py
@@ -1,18 +1,54 @@
-# -*- coding: utf-8 -*-
-import json
-import os
-from urllib.parse import urlparse
-
-import pytest
-from pyquery import PyQuery as pq
-
+import unittest
+from unittest import mock
+from slack import send_message_to_slack
 import requests
 
-from src import slack, const
 
-class TestSlackAPI:
-    @pytest.mark.parametrize('webhook', ['https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'])
-    def test_send_message_to_slack(self, webhook):　
-        r = send_message_to_slack(webhook, "ProjectRoom 1")
-        assert r.ok == True
-        assert r.status_code == 200
+class MockResponse:
+    def __init__(self, status_code):
+        self.status_code = status_code
+
+    def raise_for_status(self):
+        if 400 <= self.status_code < 500:
+            raise requests.exceptions.HTTPError('Client Error')
+        elif 500 <= self.status_code < 600:
+            raise requests.exceptions.HTTPError('Server Error')
+
+
+class test_send_message_to_slack(unittest.TestCase):
+    def setUp(self):
+        self.patcher = mock.patch('slack.requests.post')
+        self.mock_post = self.patcher.start()
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def test_send_message_to_slack(self):
+        def change_response(*args, **kwargs):
+            if args[0] == 'url_200':
+                return MockResponse(200)
+            elif args[0] == 'url_400':
+                return MockResponse(400)
+            elif args[0] == 'url_403':
+                return MockResponse(403)
+            elif args[0] == 'url_404':
+                return MockResponse(404)
+            elif args[0] == 'url_410':
+                return MockResponse(410)
+            elif args[0] == 'url_500':
+                return MockResponse(500)
+            else:
+                pass
+        self.mock_post.side_effect = change_response
+        r = send_message_to_slack('url_200', 'test')
+        self.assertEqual(r.status_code, 200)
+        with self.assertRaises(requests.exceptions.HTTPError):
+            r = send_message_to_slack('url_400', 'test')
+        with self.assertRaises(requests.exceptions.HTTPError):
+            r = send_message_to_slack('url_403', 'test')
+        with self.assertRaises(requests.exceptions.HTTPError):
+            r = send_message_to_slack('url_404', 'test')
+        with self.assertRaises(requests.exceptions.HTTPError):
+            r = send_message_to_slack('url_410', 'test')
+        with self.assertRaises(requests.exceptions.HTTPError):
+            r = send_message_to_slack('url_500', 'test')
