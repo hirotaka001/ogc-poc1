15c743b21259827cb26ca75e1abf42dad0fd25a5
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-10-26 13:54:17 +0900

    add base boilerplate to use scss and bootstrap4

--- controller/visualization/webpack.config.js
+++ controller/visualization/webpack.config.js
@@ -1,27 +1,69 @@
-const path = require('path')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: 'production',
-    entry: path.resolve(__dirname, 'js/cameraHeatmap.js'),
-    output: {
-        path: path.resolve(__dirname, 'static/js'),
-        filename: 'cameraHeatmap.combined.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                ['env', {'modules': false}]
-                            ]
+module.exports = [
+    {
+        mode: 'production',
+        entry: {
+            app: [
+                path.resolve(__dirname, 'static-src/js/cameraHeatmap.js'),
+                path.resolve(__dirname, 'static-src/css/cameraHeatmap.scss')
+            ],
+        },
+        output: {
+            path: path.resolve(__dirname, 'static/js'),
+            filename: 'cameraHeatmap.combined.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [
+                                    ['env', {modules: false}]
+                                ]
+                            }
                         }
-                    }
-                ]
-            }
-        ]
+                    ]
+                },
+                {
+                    test: /\.(scss|sass|css)$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false,
+                                importLoaders: 2
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: () => [
+                                    require('cssnano')({preset: 'default'}),
+                                    require('autoprefixer')({grid: true, browsers: ['> 1%', 'last 2 versions']})
+                                ],
+                            },
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({filename: '../css/cameraHeatmap.css'})
+        ],
+        performance: {
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+        }
     }
-};
+];
