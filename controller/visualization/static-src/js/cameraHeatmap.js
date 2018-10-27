9d5fe1ddbc9751379831ff7b7aeafe8d83116ee2
Nobuyuki Matsui <nobuyuki.matsui@gmail.com>
2018-10-27 16:36:39 +0900

    clear heatmap when pushed clear button or changed camera id

--- controller/visualization/static-src/js/cameraHeatmap.js
+++ controller/visualization/static-src/js/cameraHeatmap.js
@@ -17,105 +17,133 @@ const CAMERA_IMAGE_MAP = {
 
 class Heatmap {
     constructor() {
+        let $svg = $("svg#chart-camera");
+        this.chartWidth = $svg.width();
+        this.chartHeight = $svg.height();
+        this.tooltip = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+        this.chartArea = d3.select("svg#chart-camera");
+        this.legendArea = d3.select("svg#legend-camera");
+        this.bearer = $("input#bearer").val();
+        this.path = $("input#path").val();
         this.cameraId = "";
+        this.stVal = "";
+        this.etVal = "";
     }
 
     show() {
         console.log("Heatmap#show(), cameraId=" + this.cameraId);
-        const bearer = $("input#bearer").val();
-        const path = $("input#path").val();
-
-        let stVal = $("input#st_datetime_value").val();
-        let etVal = $("input#et_datetime_value").val();
 
         $.ajax({
             type: "GET",
-            url: path,
+            url: this.path,
             headers: {
-                Authorization: "Bearer " + bearer
+                Authorization: "Bearer " + this.bearer
             },
             data: {
-                st: formatISO8601(new Date(stVal)),
-                et: formatISO8601(new Date(etVal)),
+                camera: this.cameraId,
+                st: formatISO8601(new Date(this.stVal)),
+                et: formatISO8601(new Date(this.etVal)),
             },
             dataType: "json"
         }).then((data, status, xhr) => {
             console.log("success", data, ":", status, ":", xhr);
+
+            /* dummy data */
+            let dataset = [];
+            for (let i = 0; i < ROW; i++) {
+                for (let j = 0; j < COLUMN; j++) {
+                    switch(this.cameraId) {
+                        case "1f-1":
+                            dataset.push(i + j);
+                            break;
+                        case "1f-2":
+                            dataset.push(i * j);
+                            break;
+                        case "2f-1":
+                            dataset.push(i + 10 * j);
+                            break;
+                    }
+                }
+            }
+            this.clear();
+            this.plot(dataset)
         }).catch((xhr, status, e) => {
             console.error("error", xhr, ":", status, ":", e);
         });
+    }
 
-        let dataset = [];
-        for (let i = 0; i < ROW; i++) {
-            for (let j = 0; j < COLUMN; j++) {
-                dataset.push(i + j);
-            }
-        }
+    plot(dataset) {
+        console.log("Heatmap#plot()");
 
-        let $svg = $("svg#chart-camera");
-        let w = $svg.width();
-        let h = $svg.height();
-
-        let tooltip = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
-
-        let chart = d3.select("svg#chart-camera").selectAll("rect").data(dataset);
         let maxValue=d3.max(dataset);
         let colorScale = d3.scaleSequential(d3.interpolate("white", "red")).domain([0, maxValue]);
-        chart.enter()
-             .append("rect")
-             .attr("class", "block")
-             .attr("x", (d, i) => (i % COLUMN) * w / COLUMN)
-             .attr("y", (d, i) => Math.floor(i / COLUMN) * h / ROW)
-             .attr("width", (d, i) => w / COLUMN)
-             .attr("height", (d, i) => h / ROW)
-             .style("fill", (d, i) => colorScale(d))
-             .style("opacity", DATA_OPACITY)
-             .on("mouseover", (d) => {
-                 tooltip.transition().duration(500).style("opacity", 0);
-                 tooltip.transition().duration(200).style("opacity", 0.7);
-                 tooltip.transition().delay(1000).duration(200).style("opacity", 0);
-                 tooltip.html("<b>" + d + "</b>")
-                        .style("left", d3.event.pageX + "px")
-                        .style("top", d3.event.pageY + "px");
-             });
 
-        let legendArea = d3.select("svg#legend-camera");
-        legendArea.append("g")
-                  .attr("class", "legendSequential")
-                  .attr("transform", "translate(20,20)");
+        this.chartArea.selectAll("rect")
+                      .data(dataset)
+                      .enter()
+                      .append("rect")
+                      .attr("class", "block")
+                      .attr("x", (d, i) => (i % COLUMN) * this.chartWidth / COLUMN)
+                      .attr("y", (d, i) => Math.floor(i / COLUMN) * this.chartHeight / ROW)
+                      .attr("width", (d, i) => this.chartWidth / COLUMN)
+                      .attr("height", (d, i) => this.chartHeight / ROW)
+                      .style("fill", (d, i) => colorScale(d))
+                      .style("opacity", DATA_OPACITY)
+                      .on("mouseover", (d) => {
+                          this.tooltip.transition().duration(500).style("opacity", 0);
+                          this.tooltip.transition().duration(200).style("opacity", 0.7);
+                          this.tooltip.transition().delay(1000).duration(200).style("opacity", 0);
+                          this.tooltip.html("<b>" + d + "</b>")
+                                      .style("left", d3.event.pageX + "px")
+                                      .style("top", d3.event.pageY + "px");
+                      });
+        this.legendArea.append("g")
+                       .attr("class", "legendSequential")
+                       .attr("transform", "translate(20,20)");
         let legend = d3Legend.legendColor()
             .shapeWidth(30)
             .labelFormat(d3.format(".0f"))
             .scale(colorScale);
-        legendArea.select(".legendSequential").call(legend);
+        this.legendArea.select(".legendSequential").call(legend);
 
         d3.selectAll("svg#legend-camera rect").each((d, i, nodes) => {
             d3.select(nodes[i]).style("opacity", DATA_OPACITY);
         });
     }
+
+    clear() {
+        console.log("Heatmap#clear()");
+
+        this.chartArea.selectAll("rect").data([]).exit().remove();
+        this.legendArea.selectAll("g").data([]).exit().remove();
+    }
 }
 
 const formatISO8601 = (date) => {
     let o = date.getTimezoneOffset() / -60;
-    let offset = ((0 < o) ? '+' : '-') + ('00' + Math.abs(o)).substr(-2) + ':00';
+    let offset = ((0 < o) ? "+" : "-") + ("00" + Math.abs(o)).substr(-2) + ":00";
 
     return [
         [
             date.getFullYear(),
-            ('00' + (date.getMonth() + 1)).substr(-2),
-            ('00' + date.getDate()).substr(-2)
-        ].join('-'),
-        'T',
+            ("00" + (date.getMonth() + 1)).substr(-2),
+            ("00" + date.getDate()).substr(-2)
+        ].join("-"),
+        "T",
         [
-            ('00' + date.getHours()).substr(-2),
-            ('00' + date.getMinutes()).substr(-2),
-            ('00' + date.getSeconds()).substr(-2)
-        ].join(':'),
+            ("00" + date.getHours()).substr(-2),
+            ("00" + date.getMinutes()).substr(-2),
+            ("00" + date.getSeconds()).substr(-2)
+        ].join(":"),
         offset
-    ].join('');
+    ].join("");
 };
 
-const initFlatPickr = () => {
+const initFlatPickr = (heatmap) => {
+    let $stDatetime = $("input#st_datetime_value");
+    let $etDatetime = $("input#et_datetime_value");
+    let $showButton = $("button#show_button");
+
     let now = new Date();
     let options = {
         wrap: true,
@@ -129,19 +157,18 @@ const initFlatPickr = () => {
     flatpickr(".st_datetime", options);
     flatpickr(".et_datetime", options);
 
-    let onChangeDatetime = () => {
-        let stVal = $("input#st_datetime_value").val();
-        let etVal = $("input#et_datetime_value").val();
-        let $show_button = $("button#show_button");
+    let onChangeDatetime = (event) => {
+        heatmap.stVal = $stDatetime.val();
+        heatmap.etVal = $etDatetime.val();
 
-        if (stVal && etVal) {
-            $show_button.prop("disabled", false);
+        if (heatmap.stVal && heatmap.etVal) {
+            $showButton.prop("disabled", false);
         } else {
-            $show_button.prop("disabled", true);
+            $showButton.prop("disabled", true);
         }
     };
-    $("div.st_datetime input#st_datetime_value").on("change", onChangeDatetime);
-    $("div.et_datetime input#et_datetime_value").on("change", onChangeDatetime);
+    $stDatetime.on("change", onChangeDatetime);
+    $etDatetime.on("change", onChangeDatetime);
 };
 
 const initCameraSelector = (heatmap) => {
@@ -151,15 +178,30 @@ const initCameraSelector = (heatmap) => {
 
     $("select#camera_id").on("change", (event) => {
         heatmap.cameraId = $camera.val();
+        heatmap.clear();
         $image.attr("src", CAMERA_IMAGE_MAP[heatmap.cameraId]);
+    });
+}
+
+const initButton = (heatmap) => {
+    let $stDatetime = $("input#st_datetime_value");
+    let $etDatetime = $("input#et_datetime_value");
+    let $showButton = $("button#show_button");
+    let $clearButton = $("button#clear_button");
+
+    $showButton.on("click", (event) => heatmap.show());
+    $clearButton.on("click", (event) => {
+        heatmap.clear();
+        $stDatetime.val("");
+        $etDatetime.val("");
+        $showButton.prop("disabled", true);
     });
 }
 
 $(() => {
     let heatmap = new Heatmap();
 
-    initFlatPickr();
+    initFlatPickr(heatmap);
     initCameraSelector(heatmap);
-
-    $("button#show_button").on("click", event => heatmap.show());
+    initButton(heatmap);
 });
